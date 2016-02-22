e4f6eca0634fa75999fc028cde7ee9c2017fbe3d
Leandro Tk <leandrodtk@hotmail.com>
2016-02-22 01:48:40 -0300

    Implemented js to manage books details

--- js/books_controller.js
+++ js/books_controller.js
@@ -1,5 +1,25 @@
 var app = angular.module('myBookshelf', []);
 
 app.controller('BooksController', ['$scope', function($scope) {	
-	$scope.hi = 'Hi';
+
+	$scope.books = [
+		{ 
+			'title': 'Delivering Happiness',
+			'author': 'Tony Hsieh',
+			'url': 'http://www.amazon.com/Delivering-Happiness-Profits-Passion-Purpose/dp/160941280X',
+			'hasMediumPost': false,
+			'mediumPostUrl': '',
+			'read': false
+		},
+
+		{
+			'title': 'Elon Musk',
+			'author': 'Ashlee Vance',
+			'url': 'http://www.amazon.com/Elon-Musk-SpaceX-Fantastic-Future/dp/0062301233',
+			'hasMediumPost': false,
+			'mediumPostUrl': '',
+			'read': false
+		}
+	];
+
 }]);
